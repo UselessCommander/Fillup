4680261370b0ff74a9ecccd63821b077f1bd6d20
“UselessCommander” <Gabrielvnlausten@gmail.com>
2026-04-21 01:14:43 +0200

    fix: set Vite outDir to build to support Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Løser Vercel error
+  },
   server: {
     port: 5173,
     proxy: {
